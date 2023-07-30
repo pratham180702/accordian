5c7b4672ff55615cdb0e59dba5a6f9ef30f70f78
unknown <prathamkakkad18@gmail.com>
2023-07-30 19:37:18 +0530

    final changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
-  let ans = document.querySelectorAll(".ans")[i];
-  let contentItems = document.querySelectorAll(".content-items")[i];
+  let ans = document.querySelectorAll(".initial-content")[i];
   let quesHeading = document.querySelectorAll(".content-items h3")[i];
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     var btnContent = this.innerHTML;
@@ -10,13 +9,11 @@ for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
     setTimeout(() => {
       if (btnContent === "+") {
         this.textContent = "‒";
-        ans.style.maxHeight = "200px";
-        ans.style.padding = "10px";
+        ans.classList.add("ans");
         quesHeading.classList.add("h3-active");
       } else if (btnContent === "‒") {
         this.textContent = "+";
-        ans.style.maxHeight = "0";
-        ans.style.padding = "0";
+        ans.classList.remove("ans");
         quesHeading.classList.remove("h3-active");
       }
 
