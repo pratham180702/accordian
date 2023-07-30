9c7a955a99008369c00162c08751b9d5eb982069
unknown <prathamkakkad18@gmail.com>
2023-07-30 19:32:17 +0530

    changes added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
+  let ans = document.querySelectorAll(".ans")[i];
+  let contentItems = document.querySelectorAll(".content-items")[i];
+  let quesHeading = document.querySelectorAll(".content-items h3")[i];
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     var btnContent = this.innerHTML;
 
@@ -7,11 +10,14 @@ for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
     setTimeout(() => {
       if (btnContent === "+") {
         this.textContent = "‒";
+        ans.style.maxHeight = "200px";
+        ans.style.padding = "10px";
+        quesHeading.classList.add("h3-active");
       } else if (btnContent === "‒") {
         this.textContent = "+";
-        // var ans = document.querySelectorAll(".ans")[i];
-        // ans.style.maxHeight = "0";
-        // ans.style.padding = "0";
+        ans.style.maxHeight = "0";
+        ans.style.padding = "0";
+        quesHeading.classList.remove("h3-active");
       }
 
       this.classList.remove("active");
